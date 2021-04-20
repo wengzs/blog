93e1837847a118ca0c7e6bb5e0f63f52196427d8
wengzhisong <1322457349@qq.com>
2021-04-20 17:16:57 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ git commit -m "deploy ${time}"
 git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
 
 cd ../../
+git config user.email "1322457349@qq.com"
+git config user.name "wengzhisong"
 git add .
 git commit -m 'deploy'
 git checkout dev || git checkout -b dev
