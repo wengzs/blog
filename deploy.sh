ec9f37f2fa58b90567a7bae0ce3219b1e84912fd
wengzhisong <1322457349@qq.com>
2021-04-20 17:18:49 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 set -e
 
-git config user.email "1322457349@qq.com"
-git config user.name "wengzhisong"
+git config --global user.email "1322457349@qq.com"
+git config --global user.name "wengzhisong"
 
 git add .
 git commit -m 'deploy'
@@ -16,12 +16,15 @@ git commit -m "deploy ${time}"
 git push -f git@github.com:wengzhisong-hz/wengzhisong-hz.github.io.git master
 
 cd ../../
-git config user.email "1322457349@qq.com"
-git config user.name "wengzhisong"
+
 git add .
 git commit -m 'deploy'
 git checkout dev || git checkout -b dev
 git merge master
 git push origin dev
 git checkout master
+
+git config --global user.name "sp0140(陆抗)"
+git config --global user.email "wengzs@supaur.com"
+
 cd -
