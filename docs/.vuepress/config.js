e7356e145fd03852f198a538c474d5bbf8a541a2
wengzhisong <1322457349@qq.com>
2021-06-01 12:43:39 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    title: '宁静致远',
-    description: '宁静致远',
+    title: 'Aiur',
+    description: '',
     plugins: {
         'sitemap': {
             hostname: 'https://wengzhisong-hz.github.io'
@@ -14,31 +14,26 @@ module.exports = {
         sidebar: [
             {
                 title: '基础',
-                path: '/basic/',
                 collapsable: false,
                 sidebarDepth: 2,
                 children: [
                     {
                         title: 'C++',
-                        path: '/basic/c++/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Electron',
-                        path: '/basic/electron/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Java',
-                        path: '/basic/java/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'JavaScript',
-                        path: '/basic/javascript/',
                         collapsable: false,
                         children: [
                             {
@@ -57,7 +52,6 @@ module.exports = {
                     },
                     {
                         title: 'Linux',
-                        path: '/basic/linux/',
                         collapsable: false,
                         children: [
                             {
@@ -68,25 +62,21 @@ module.exports = {
                     },
                     {
                         title: 'Mongodb',
-                        path: '/basic/mongodb/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'MySQL',
-                        path: '/basic/mysql/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Shell',
-                        path: '/basic/shell/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Webpack',
-                        path: '/basic/webpack/',
                         collapsable: false,
                         children: []
                     }
@@ -94,43 +84,36 @@ module.exports = {
             },
             {
                 title: '深入',
-                path: '/deepin/',
                 collapsable: false,
                 sidebarDepth: 2,
                 children: [
                     {
                         title: 'Koa',
-                        path: '/deepin/koa/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Nest',
-                        path: '/deepin/nest/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Node',
-                        path: '/deepin/node/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'React',
-                        path: '/deepin/react/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Spring',
-                        path: '/deepin/spring/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'Vue',
-                        path: '/deepin/vue/',
                         collapsable: false,
                         children: []
                     }
@@ -144,43 +127,31 @@ module.exports = {
                 children: [
                     {
                         title: 'Authorization',
-                        path: '/topic/authorization/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: 'CMS',
-                        path: '/topic/cms/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '跨端',
-                        path: '/topic/cross-platform/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '前端基建',
-                        path: '/topic/fe-infrastructure/',
-                        collapsable: false,
-                        children: []
-                    },
-                    {
-                        title: '手写代码',
-                        path: '/topic/handwriting/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '直播',
-                        path: '/topic/live-streaming/',
                         collapsable: false,
                         children: []
                     },
                     {
-                        title: '监控平台',
-                        path: '/topic/log/',
+                        title: '监控',
                         collapsable: false,
                         children: [
                             {
@@ -191,25 +162,21 @@ module.exports = {
                     },
                     {
                         title: '低代码',
-                        path: '/topic/low-code/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '主数据',
-                        path: '/topic/main-data/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '微前端',
-                        path: '/topic/micro-fe/',
                         collapsable: false,
                         children: []
                     },
                     {
                         title: '用户中心',
-                        path: '/topic/user-center/',
                         collapsable: false,
                         children: []
                     }
@@ -217,7 +184,6 @@ module.exports = {
             },
             {
                 title: '管理',
-                path: '/management/',
                 collapsable: false,
                 sidebarDepth: 1,
                 children: [
@@ -233,7 +199,6 @@ module.exports = {
             },
             {
                 title: '观点',
-                path: '/opinion/',
                 collapsable: false,
                 sidebarDepth: 1,
                 children: [
